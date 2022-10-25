320a014fadd23cd6cc69c3dc12c399aad829d898
Artem <temfedorenko@gmail.com>
2022-10-25 18:56:22 +0300

    add static layout

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Облік співробітників компанії StopFailing</h1>
+      <h2>Загальна кількість співробітників: </h2>
+      <h2>Бонус отримають: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
